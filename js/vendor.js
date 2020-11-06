2da6035b3853722629bf4b73898a1aa80f9351d3
ManuelWandeto <immanuelmojo@hotmail.com>
2020-11-06 16:14:52 +0300

    make vendor.js second entrypoint

---
+++ js/vendor.js
@@ -0,0 +1,12 @@
+//vendor css
+require('../css/base.css');
+require('../css/vendor.css');
+require('intl-tel-input/build/css/intlTelInput.css');
+
+//vendor javascript
+require('./modernizr');
+require('./pace.min')
+const $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
+require('./plugins');
